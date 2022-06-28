f4bc22adfec1c4100f8f48f93c86ac05ff719589
keermanish <keermanish@gmail.com>
2022-06-28 14:55:37 +0530

    ENV var setup added

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
+        <p>You are running in {import.meta.env.VITE_APP_MODE} mode</p>
         <p>
           <button type="button" onClick={() => setCount(count => count + 1)}>
             count is: {count}
